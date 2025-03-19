0a553622bcc30f26df3f6455e15bd96fb6c0fc40
JOAO VITOR ALVES ESCUDEIRO <unifjescudeiro@feinet.fei.edu.br>
2025-03-19 16:31:34 -0300

    Finalizando o main.c

--- main.c
+++ main.c
@@ -16,9 +16,29 @@ do{
     scanf("%d", &opcao);
 
     if(opcao == 0){}
-    else if(opcao == 1){}
-    else if(opcao == 2){}
-    else if(opcao == 3){}
+    else if(opcao == 1){
+        codigo=criarTarefa(&lt);
+        if(codigo==1){
+        printf("Erro ao criar tarefa: sem espaco disponivel.\n");
+        }
+    else if(opcao == 2){
+         codigo=deletarTarefa(&lt);
+        if(codigo==1){
+            printf("Erro ao deletar tarefa: nao existem tarefas para deletar.\n");
+        }
+        if(codigo==2){
+            printf("Erro ao deletar tarefa: posicao invalida.\n");
+        }
+    }
+    else if(opcao == 3){
+        codigo=listarTarefas(&lt);
+        if(codigo==1){
+            printf("Erro ao listar tarefas: nao existem tarefas para serem listadas.\n");
+        }
+        else{
+            print("Opcao invalida.\n");
+        }
+    }
     else {printf("Opcao invalida\n");}
 }while(opcao!=0);
 
